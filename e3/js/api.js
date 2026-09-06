b9b1f90f7c965830060c67404795c0dbb1023445
Aggak <linnek.dev@gmail.com>
2026-09-06 14:56:54 -0300

    Carregamento das tarefas com fetch

--- e3/js/api.js
+++ e3/js/api.js
@@ -1,3 +1,8 @@
-function carregarTarefas() {
-  // Lógica para carregar tarefas
+export async function carregarTarefas() {
+  const resposta = await fetch("./dados.json");
+  if (!resposta.ok) {
+    throw new Error("Erro HTTP: " + resposta.status);
+  }
+  const dados = await resposta.json();
+  return dados.tarefas;
 }
