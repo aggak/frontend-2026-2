c57e84b01802e773b8d2ffee50e1d839fc1b2603
Aggak <linnek.dev@gmail.com>
2026-09-06 18:55:10 -0300

    Alguns ajustes de coisas sem uso

--- e3/js/app.js
+++ e3/js/app.js
@@ -1,8 +1,5 @@
-import { renderizarTarefas } from "./renderizacao.js";
 import { carregarTarefas } from "./api.js";
 import { renderizarEstado } from "./estados.js";
-
-const quadro = document.querySelector(".quadro");
 
 async function iniciar() {
   renderizarEstado("carregando", []);
