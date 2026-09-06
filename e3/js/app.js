2dd1d89ee4cbef649a4c09273990db4c9cceb5e7
Aggak <linnek.dev@gmail.com>
2026-09-06 15:24:04 -0300

    Exibição da mensagem de carregamento na página

--- e3/js/app.js
+++ e3/js/app.js
@@ -1,3 +1,5 @@
-import { criarCartao, renderizarTarefas } from './renderizacao.js';
+import { criarCartao, renderizarTarefas } from "./renderizacao.js";
+import { renderizarEstado } from "./estados.js";
 
-const quadro = document.querySelector('.quadro');
+const quadro = document.querySelector(".quadro");
+renderizarEstado("carregando", []);
